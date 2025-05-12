88924149504c4c54a8d508fe8035a882119cd5d5
Brady Deroy <b.deroy@vehikl.com>
2025-05-12 16:27:47 -0400

    create basic input and button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+const item = ref<string>('')
+const addItem = () => {
+  console.log('Item', item.value)
+}
 </script>
 
 <template>
   <main>
+    <input type="text" v-model="item" placeholder="Enter something" />
+    <button type="button" @click="addItem">Add Item</button>
   </main>
 </template>

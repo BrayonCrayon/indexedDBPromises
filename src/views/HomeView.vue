855769bf1a9a12da4378ba201cfa4b6a28bfefa0
Brady Deroy <b.deroy@vehikl.com>
2025-05-13 16:32:48 -0400

    displayed all item records in indexDB store
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    Co-authored-by: Xiao X <x.xue@vehikl.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { getConstantValue } from "typescript";
 import {ref} from "vue";
 
 const db = ref<IDBDatabase|null>(null);
+const items = ref<{id: number, value: string}[]>([])
 const itemStore = ref<IDBObjectStore>();
 
 const item = ref<string>('')
@@ -15,6 +17,13 @@ DBOpenRequest.onsuccess = (event) => {
   console.log('Success opening DB')
 
   db.value = DBOpenRequest.result;
+
+  // get items here
+  itemStore.value = db.value.transaction(['items']).objectStore('items');
+  const allItemsRequest = itemStore.value.getAll();
+  allItemsRequest.onsuccess = (event) => {
+    items.value = allItemsRequest.result
+  }
 };
 
 DBOpenRequest.onupgradeneeded = (event: IDBVersionChangeEvent) => {
@@ -51,5 +60,13 @@ const addItem = () => {
   <main>
     <input type="text" v-model="item" placeholder="Enter something" />
     <button type="button" @click="addItem">Add Item</button>
+    <br/>
+    <template v-for="item in items" :key="item.id">
+      <div>
+        <p style="{color: white}">{{ item.value }}</p>
+        <button> edit </button>
+      </div>
+      <br/>
+    </template>
   </main>
 </template>

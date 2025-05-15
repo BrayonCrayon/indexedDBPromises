784a2a42ad1857545e8082d33ab739c36a33edbd
BrayonDeroy <b.deroy@vehikl.com>
2025-05-15 16:12:58 -0400

    Indexdb CRUD Functionality (#1)
    * create basic input and button
    
    * create item store in indexedDB
    
    * add a record into the db
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    Co-authored-by: Xiao X <x.xue@vehikl.com>
    
    * displayed all item records in indexDB store
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    Co-authored-by: Xiao X <x.xue@vehikl.com>
    
    * update an item records in indexDB store
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    Co-authored-by: Xiao X <x.xue@vehikl.com>
    
    * Delete from the indexdb store
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    
    * Make it reactive
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    
    * remove global itemStore
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    
    ---------
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    Co-authored-by: Xiao X <x.xue@vehikl.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,105 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+const db = ref<IDBDatabase|null>(null);
+const items = ref<{id: number, value: string}[]>([])
+
+const item = ref<string>('')
+const DBOpenRequest = window.indexedDB.open('promises')
+
+DBOpenRequest.onerror = (event) => {
+  console.log('Error opening DB')
+};
+
+DBOpenRequest.onsuccess = (event) => {
+  console.log('Success opening DB')
+
+  db.value = DBOpenRequest.result;
+
+  retrieveAllItems();
+};
+
+DBOpenRequest.onupgradeneeded = (event: IDBVersionChangeEvent) => {
+  db.value = event.target!.result;
+
+  db.value!.onerror = (event) => {
+    console.log('Error loading database.');
+  };
+
+  const itemStore = db.value!.createObjectStore('items', { keyPath: 'id', autoIncrement: true });
+  itemStore.createIndex('items', 'items', { unique: false });
+};
+
+const retrieveAllItems = () => {
+  const itemStore = db.value!.transaction(['items']).objectStore('items');
+
+  const allItemsRequest = itemStore.getAll();
+
+  allItemsRequest.onsuccess = (event) => {
+    items.value = allItemsRequest.result
+  }
+}
+
+const addItem = () => {
+  if (!db.value) return;
+
+  const transaction = db.value.transaction(['items'], 'readwrite')
+
+  transaction.oncomplete = () => {
+    item.value = "";
+  };
+
+  const itemStore = transaction.objectStore('items');
+
+  const objectStoreRequest = itemStore.add({ value: item.value });
+
+  objectStoreRequest.onerror = (event) => {
+    console.error('Could not create the item.', objectStoreRequest.result);
+  }
+  retrieveAllItems();
+}
+
+const updateItem = (item: {id: number, value: string}) => {
+  if (!db.value) return;
+
+  const transaction = db.value.transaction(['items'], 'readwrite')
+
+  transaction.oncomplete = () => {
+    console.log('Transaction complete')
+  };
+
+  // new read write
+  const itemStore = transaction.objectStore('items');
+
+  const objectStoreRequest = itemStore.put({value: item.value, id: item.id});
+
+  objectStoreRequest.onerror = (event) => {
+    console.error('Could not update the item.', objectStoreRequest.result);
+  }
+}
+
+const deleteItem = (id: number) => {
+  const transaction = db.value!.transaction(['items'], 'readwrite')
+
+  const objectStoreRequest = transaction.objectStore('items').delete(id);
+
+  objectStoreRequest.onerror = (event) => {
+    console.error('Could not delete the item.', objectStoreRequest.result);
+  }
+  retrieveAllItems();
+}
 </script>
 
 <template>
   <main>
+    <input type="text" v-model="item" placeholder="Enter something" />
+    <button type="button" @click="addItem">Add Item</button>
+
+    <h3>Items in Database</h3>
+    <div v-for="promisedItem in items" :key="promisedItem.id">
+      <input type="text" v-model="promisedItem.value">
+      <button @click="updateItem(promisedItem)"> update </button>
+      <button @click="deleteItem(promisedItem.id)"> delete </button>
+    </div>
   </main>
 </template>

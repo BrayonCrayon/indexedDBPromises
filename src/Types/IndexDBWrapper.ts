81155663b8ce4584360669c578214270dcf2d829
BrayonDeroy <b.deroy@vehikl.com>
2025-06-03 15:55:23 -0400

    Feature/typing and wrapping (#3)
    * Create Item type and clean up some typings
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    
    * Create wrapper class to encapsulate functionality
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    
    * Create wrapper class to encapsulate functionality
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    
    * WIP
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    
    * fix get all items bug
    
    ---------
    
    Co-authored-by: Nick W <n.whitford@vehikl.com>
    Co-authored-by: Kate S <e.stroganova@vehikl.com>
    Co-authored-by: Kate <98836466+heyitskei@users.noreply.github.com>

---
+++ src/Types/IndexDBWrapper.ts
@@ -0,0 +1,94 @@
+export class IndexDBWrapper {
+  db: IDBDatabase|null = null
+
+  open(name: string) {
+    return new Promise((resolve, reject) => {
+      const DBOpenRequest = window.indexedDB.open(name)
+
+      DBOpenRequest.onerror = () => {
+        reject(DBOpenRequest.result)
+      };
+
+      DBOpenRequest.onsuccess = () => {
+        this.db = DBOpenRequest.result;
+        resolve(DBOpenRequest.result)
+      };
+
+      DBOpenRequest.onupgradeneeded = () => {
+        this.db = DBOpenRequest.result;
+        this.db!.onerror = () => {
+          reject(DBOpenRequest.result)
+        };
+        const itemStore = this.db!.createObjectStore('items', {keyPath: 'id', autoIncrement: true});
+
+        itemStore.createIndex('items', 'items', {unique: false});
+
+        resolve(DBOpenRequest.result);
+      };
+    });
+  }
+
+  add<T extends object>(item: T) {
+    if (!this.db) return;
+
+    const transaction = this.db.transaction(['items'], 'readwrite')
+
+    const itemStore = transaction.objectStore('items');
+
+    const objectStoreRequest = itemStore.add({ ...item });
+
+    objectStoreRequest.onerror = () => {
+      console.error('Could not create the item.', objectStoreRequest.result);
+    }
+  }
+
+  update<T extends object>(item: T) {
+    if (!this.db) return;
+
+    const transaction = this.db.transaction(['items'], 'readwrite')
+
+    transaction.oncomplete = () => {
+      console.log('Transaction complete')
+    };
+
+    const itemStore = transaction.objectStore('items');
+
+    const objectStoreRequest = itemStore.put({ ...item });
+
+    objectStoreRequest.onerror = () => {
+      console.error('Could not update the item.', objectStoreRequest.result);
+    }
+  }
+
+  delete(key: IDBValidKey|IDBKeyRange) {
+    if (!this.db) return;
+
+    const transaction = this.db.transaction(['items'], 'readwrite')
+
+    const objectStoreRequest = transaction.objectStore('items').delete(key);
+
+    objectStoreRequest.onerror = () => {
+      console.error('Could not delete the item.', objectStoreRequest.result);
+    }
+  }
+
+  getAll<T>(): T[] {
+    return new Promise((resolve, reject) => {
+      if (!this.db) {
+        reject('No db found')
+      }
+
+      const itemStore = this.db.transaction(['items']).objectStore('items');
+
+      const allItemsRequest = itemStore.getAll();
+
+      allItemsRequest.onsuccess = () => {
+        resolve(allItemsRequest.result);
+      }
+
+      allItemsRequest.onerror = () => {
+        reject(allItemsRequest.result)
+      }
+    });
+  }
+}

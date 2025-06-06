235e7737421f3ec5d7444b67e8bcec22d6a0d68b
Brady Deroy <b.deroy@vehikl.com>
2025-06-06 16:27:27 -0400

    - Fully test rendering out items in test
    Co-authored-by: Ekaterina Stroganova <e.stroganova@vehikl.com>

--- src/tests/HomeView.spec.ts
+++ src/tests/HomeView.spec.ts
@@ -1,10 +1,11 @@
 // vi.hoisted
 // vi.mock
-import {describe, it, expect, vi, type Mocked} from "vitest";
-import {mount} from "@vue/test-utils";
+import {describe, it, expect, vi, type Mocked, beforeEach} from "vitest";
+import {flushPromises, mount} from "@vue/test-utils";
 import HomeView from "@/views/HomeView.vue";
 import itemFactory from "@/tests/factories/itemFactory.ts";
 import Wrapper, {IndexDBWrapper} from "@/Types/IndexDBWrapper.ts";
+import type {Item} from "@/Types/Item.ts";
 
 const mockedIndexedWrapper = new IndexDBWrapper()
 
@@ -12,26 +13,69 @@ export const { db } = Wrapper as Mocked<typeof Wrapper>;
 vi.mock("@/Types/IndexDBWrapper.ts");
 
 describe("HomeView", () => {
+  beforeEach(() => {
+    mockedIndexedWrapper.open.mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    })
+    db.mockImplementation(() => mockedIndexedWrapper);
+  })
+
   it("renders the HomeView component", () => {
     const wrapper = mount(HomeView);
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  it("retrieves data on initial load", () => {
+  it("retrieves data on initial load", async () => {
     const items = [];
     for (let i = 0; i < 10; i++) {
       items.push(itemFactory())
     }
-    // TODO: We are here
-    mockedIndexedWrapper.open.mockImplementation(() => {
-      return new Promise((resolve) => { resolve(); });
+    mockedIndexedWrapper.getAll.mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve(items);
+      })
     })
-    db.mockImplementation(() => mockedIndexedWrapper);
+
     const wrapper = mount(HomeView);
-    // call the open() on db wrapper
+    await flushPromises();
 
-    // that should retrieve items
+    items.forEach(item => {
+      expect(wrapper.find(`[id="${item.id}-input"]`).exists()).toBeTruthy();
+    })
+  });
 
-    // expect items to be there
+  it("will add a new item to our list and retrieve all items", async () => {
+    const item = itemFactory();
+    mockedIndexedWrapper.getAll.mockImplementation(() => {
+      return new Promise((resolve) => {
+        console.log("populated stuff")
+        resolve([item]);
+      })
+    })
+    mockedIndexedWrapper.getAll.mockImplementationOnce(() => {
+      return new Promise((resolve) => {
+        console.log("empty stuff")
+        resolve([]);
+      })
+    })
+    mockedIndexedWrapper.add.mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve(item);
+      })
+    })
+
+    const wrapper = mount(HomeView);
+
+    const addInput = wrapper.find('[id="add-input"]')
+    await addInput.setValue(item.value)
+
+    const addButton = wrapper.find('[id="add-button"]')
+    await addButton.trigger('click');
+
+    await flushPromises();
+
+    expect(wrapper.find(`[id="${item.id}-input"]`).exists()).toBeTruthy();
   });
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import type {Item} from "@/Types/Item.ts";
-import {IndexDBWrapper, db} from "@/Types/IndexDBWrapper.ts";
+import {db} from "@/Types/IndexDBWrapper.ts";
 
 const items = ref<Item[]>([])
 const item = ref<string>('')
 const indexDBWrapper = db()
-console.log("in component", indexDBWrapper)
+
 indexDBWrapper.open('promises')
   .then(() => {retrieveAllItems()})
   .catch((err) => {console.log(err)})
@@ -22,7 +22,7 @@ const retrieveAllItems = () => {
 const addItem = () => {
   indexDBWrapper.add({value: item.value})
     .then((response: Item) => {
-      alert(`Item added successfully! Id: ${response.id}, Value: ${response.value}`)
+      console.log(`Item added successfully! Id: ${response.id}, Value: ${response.value}`)
       item.value = "";
       retrieveAllItems();
     })
@@ -32,7 +32,7 @@ const addItem = () => {
 const updateItem = (item: Item) => {
   indexDBWrapper.update(item)
     .then((updatedItemId: number) => {
-      alert(`Item under id ${updatedItemId} was updated successfully`)
+      console.log(`Item under id ${updatedItemId} was updated successfully`)
     })
     .catch((err) => {console.log(err)});
 }
@@ -41,7 +41,7 @@ const deleteItem = (id: number) => {
   indexDBWrapper.delete(id)
     .then(() => {
       console.log(id);
-      alert(`Item under id ${id} was deleted successfully`)
+      console.log(`Item under id ${id} was deleted successfully`)
       retrieveAllItems();
     })
     .catch((err) => {console.log(err)});
@@ -50,12 +50,12 @@ const deleteItem = (id: number) => {
 
 <template>
   <main>
-    <input type="text" v-model="item" placeholder="Enter something" />
-    <button type="button" @click="addItem">Add Item</button>
+    <input id="add-input" type="text" v-model="item" placeholder="Enter something" />
+    <button id="add-button" type="button" @click="addItem">Add Item</button>
 
     <h3>Items in Database</h3>
     <div v-for="promisedItem in items" :key="promisedItem.id">
-      <input type="text" v-model="promisedItem.value">
+      <input :id="`${promisedItem.id}-input`" type="text" v-model="promisedItem.value">
       <button @click="updateItem(promisedItem)"> update </button>
       <button @click="deleteItem(promisedItem.id)"> delete </button>
     </div>
